920eb7837f1c291d9e435f52d57c3a3c05d81edf
Bikramjeet Singh <cvichiee@protonmail.ch>
2019-12-15 17:51:30 +0530

    Character counter
    Program to get the number of each character in a string, using a dictionary as counter.

---
+++ char_count.py
@@ -0,0 +1,10 @@
+# program to get the number of each character in a string.
+
+message = 'It was a dull cold day in April and the clock was striking 5'
+count = {} # dictionary as character counter
+
+for char in message.lower():
+    count.setdefault(char, 0) # if character doesn't already exist, start its counter from 0
+    count[char] += 1
+
+print(count)
